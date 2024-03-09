f592132deb3abaf1378b78289a757bef5d7be835
Aaron Ma <aaronma113@gmail.com>
2024-03-08 16:49:36 -0800

    Added interface level comment to Writable interface

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -2,6 +2,7 @@ package persistence;
 
 import org.json.JSONObject;
 
+// Represents ah interface with writable to Json methods
 public interface Writable {
 
     // EFFECTS: returns this as JSON object
